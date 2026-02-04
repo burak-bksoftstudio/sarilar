137e962e18865f8c37d9101d987229cf8d871685
burak-bksoftstudio <burak@bksoftstudio.com>
2026-02-04 18:47:39 +0300

    prepare for custom subdomain

--- next.config.ts
+++ next.config.ts
@@ -1,17 +1,13 @@
 import type { NextConfig } from "next";
-import createNextIntlPlugin from 'next-intl/plugin';
+import createNextIntlPlugin from "next-intl/plugin";
 
-const withNextIntl = createNextIntlPlugin('./src/i18n/request.ts');
+const withNextIntl = createNextIntlPlugin("./src/i18n/request.ts");
 
 const nextConfig: NextConfig = {
+  output: "export",
+
   images: {
-    formats: ['image/avif', 'image/webp'],
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'www.sarilarboya.com.tr',
-      },
-    ],
+    unoptimized: true,
   },
 };
 
